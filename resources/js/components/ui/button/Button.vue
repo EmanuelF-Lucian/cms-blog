cbc10f99eeb44bdb3f67f934b15d72c2b8ebc1cf
EmanuelF-Lucian <emanuellucian.f@gmail.com>
2026-03-03 14:44:32 +0200

    fix: shadcn button

--- resources/js/components/ui/button/Button.vue
+++ resources/js/components/ui/button/Button.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
 import type { PrimitiveProps } from "reka-ui"
-import type { ButtonHTMLAttributes, HTMLAttributes } from "vue"
+import type { HTMLAttributes } from "vue"
 import type { ButtonVariants } from "."
 import { Primitive } from "reka-ui"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "."
 
-interface Props extends PrimitiveProps, ButtonHTMLAttributes {
+interface Props extends PrimitiveProps {
   variant?: ButtonVariants["variant"]
   size?: ButtonVariants["size"]
-  
+  class?: HTMLAttributes["class"]
 }
-defineOptions({
-  inheritAttrs: false,
-})
 
 const props = withDefaults(defineProps<Props>(), {
   as: "button",
@@ -22,7 +19,9 @@ const props = withDefaults(defineProps<Props>(), {
 
 <template>
   <Primitive
-    v-bind="$attrs"
+    data-slot="button"
+    :data-variant="variant"
+    :data-size="size"
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
